a71168135f5aa46721da7c00569559205b9bcc33
virus77z9x <virus77z9x@localhost.localdomain>
2025-11-18 20:44:34 +0200

    login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         //
         $middleware->alias([
             'auth' => App\Http\Middleware\Authenticate::class,
-        ])
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
